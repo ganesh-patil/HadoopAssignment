09bd8d964ae59d4432ef6e039e9075b754370b40
ganeshp-c <ganeshp@surveymonkey.com>
2016-12-05 19:40:19 +0530

    task 4 added final output and hive queries

---
+++ task4/udf/checkDateRange2.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.PigException;
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.DataType;
+import org.apache.pig.builtin.*;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+import java.text.ParseException;
+
+/**
+ * Determine whether a bag or map is empty.
+ */
+public class checkDateRange2 extends FilterFunc {
+
+    @Override
+    public Boolean exec(Tuple input) throws IOException {
+          SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+           String  manage_from =  (String)input.get(0);
+           String   manage_to =  (String)input.get(1);
+           String  emp_from =  (String)input.get(2);
+           String   emp_to =  (String)input.get(3);
+            Date manager_from_date = formatter.parse(manage_from.replaceAll("Z$", "+0000"));
+            Date manager_to_date = formatter.parse(manage_to.replaceAll("Z$", "+0000"));
+            Date emp_from_date = formatter.parse(emp_from.replaceAll("Z$", "+0000"));
+            Date emp_to_date = formatter.parse(emp_to.replaceAll("Z$", "+0000"));
+
+           if (((emp_from_date.compareTo(manager_from_date) > 0) && emp_from_date.compareTo(manager_to_date) < 0) || emp_from_date.compareTo(manager_from_date) == 0) {
+               return true;
+           }
+           else if(emp_from_date.compareTo(manager_from_date) < 0 && emp_to_date.compareTo(manager_from_date) > 0 ){
+               return true;
+           }
+         //  else if(emp_from_date.compareTo(manager_from_date) > 0  && emp_to_date.compareTo(manager_to_date) > 0) {
+          //     return true;
+          //  }
+
+
+          
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+  return false;              
+}            
+} 
